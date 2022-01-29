2dcda2b8236954289fac153575788062493b3ea2
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-29 13:01:59 +0500

    cdk app

---
+++ step24_lambda_layers/example01_lambda_with_multiple_layers/test/example01_lambda_with_multiple_layers.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Example01LambdaWithMultipleLayers from '../lib/example01_lambda_with_multiple_layers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example01_lambda_with_multiple_layers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example01LambdaWithMultipleLayers.Example01LambdaWithMultipleLayersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

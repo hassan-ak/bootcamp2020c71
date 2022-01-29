9fd523fda635f3dbdd2c99a019c3c54b68b171ee
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-29 12:52:46 +0500

    Update example00_lambda_with_single_layer-stack.ts

--- step24_lambda_layers/example00_lambda_with_single_layer/lib/example00_lambda_with_single_layer-stack.ts
+++ step24_lambda_layers/example00_lambda_with_single_layer/lib/example00_lambda_with_single_layer-stack.ts
@@ -1,15 +1,22 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class Example00LambdaWithSingleLayerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lambda layer
+    const lambdaLayer = new lambda.LayerVersion(this, 'LambdaLayer', {
+      code: lambda.Code.fromAsset('lambda-layer'),
+      // compatibleRuntimes: [lambda.Runtime.NODEJS_14_X],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Example00LambdaWithSingleLayerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda function
+    new lambda.Function(this, 'LambdaWithLambdaLayer', {
+      runtime: lambda.Runtime.NODEJS_14_X, // execution environment
+      code: lambda.Code.fromAsset('lambda-fns'), // code loaded from the "lambda" directory
+      handler: 'lambda.handler', // file is "lambda", function is "handler"
+      layers: [lambdaLayer],
+    });
   }
 }
